f9c72852065cf540da9b6c34811b59b50e200bb0
paddingdun <gaokun_xf@126.com>
2015-11-30 16:05:46 +0800

    excel工具类

--- src/main/java/me/paddingdun/util/DateHelper.java
+++ src/main/java/me/paddingdun/util/DateHelper.java
@@ -1,6 +1,13 @@
 package me.paddingdun.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+
+import com.mysql.jdbc.util.TimezoneDump;
 
 /**
  * 
@@ -10,9 +17,34 @@ import java.util.Date;
  */
 public class DateHelper {
 
+	/**
+	 * default date format: year-month-day hour:minute:second
+	 * 默认日期格式(日期和时间);
+	 */
 	public final static String DATE_FMT_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+	
+	/**
+	 * date format1;
+	 * 日期格式;
+	 */
+	public final static String DATE_FMT_1 = "yyyy-MM-dd";
+	
+	
+	/**
+	 * format the date to special str;
+	 * @param date
+	 * @param pattern
+	 * @return
+	 */
+	public static String format(Date date, String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		return sdf.format(date);
+	}
 	
 	public static Date now(){
 		return new Date();
+	}
+	
+	public static void main(String[] args) {
 	}
 }
