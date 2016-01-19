c3da6cfc8d1d3bd20083a8eb0a4b1a3dd6ec03e7
paddingdun <gaokun_xf@126.com>
2016-01-19 14:42:21 +0800

    分页修正含有datatables传递参数!

--- src/main/java/me/paddingdun/data/paging/Paging.java
+++ src/main/java/me/paddingdun/data/paging/Paging.java
@@ -1,6 +1,8 @@
 package me.paddingdun.data.paging;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * paging 对象;
@@ -28,5 +30,55 @@ public class Paging implements Serializable{
 	}
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
+	}
+	
+	/**
+	 * datatables 查询参数;
+	 */
+	private Integer start = 0;
+	private Integer length = 10;
+
+	public Integer getStart() {
+		return start;
+	}
+
+	public void setStart(Integer start) {
+		this.start = start;
+	}
+
+	public Integer getLength() {
+		return length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+
+	private List<Map<ColumnCriterias, String>> columns;
+	private List<Map<OrderCriterias, String>> order;
+	private Map<SearchCriterias, String> search;
+
+	public List<Map<ColumnCriterias, String>> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<Map<ColumnCriterias, String>> columns) {
+		this.columns = columns;
+	}
+
+	public List<Map<OrderCriterias, String>> getOrder() {
+		return order;
+	}
+
+	public void setOrder(List<Map<OrderCriterias, String>> order) {
+		this.order = order;
+	}
+
+	public Map<SearchCriterias, String> getSearch() {
+		return search;
+	}
+
+	public void setSearch(Map<SearchCriterias, String> search) {
+		this.search = search;
 	}
 }
